bacb961c8f31086f22515d4a8ce0f29a3e1dbffa
Eric Higbee <erichigbee@gmail.com>
2025-09-30 19:52:44 -0700

    Test direct FTP deploy

---
+++ server/deploy.js
@@ -0,0 +1,18 @@
+const ftpDeploy = require("ftp-deploy");
+const ftp = new ftpDeploy();
+
+const config = {
+  user: process.env.FTP_USERNAME,
+  password: process.env.FTP_PASSWORD,
+  host: process.env.FTP_HOST,
+  port: 21,
+  localRoot: __dirname + "/client/build",
+  remoteRoot: "/public_html/ourlivingneighborhoods",
+  include: ["*", "**/*"],
+  deleteRemote: true,
+  forcePasv: true,
+};
+
+ftp.deploy(config)
+  .then(() => console.log("FTP deploy finished"))
+  .catch(err => console.error("FTP deploy failed:", err));
