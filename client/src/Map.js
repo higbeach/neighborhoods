5e354a0378f957230469d9819ac95a980e9f1e32
Eric Higbee <erichigbee@gmail.com>
2025-10-17 19:55:08 -0700

    oops

--- client/src/Map.js
+++ client/src/Map.js
@@ -26,7 +26,7 @@ const NeighborhoodMap = () => {
   const [showSurveyForm, setShowSurveyForm] = useState(false);
   const [surveyComplete, setSurveyComplete] = useState(false);
 
-  const [drawingStarted] = useState(false);
+  const [drawingStarted, setDrawingStarted] = useState(false);
 
   useEffect(() => {
     if (mapRef.current) return;
@@ -264,21 +264,43 @@ const startOver = () => {
         </div>
       )}
 
-      {step === 3 && drawingStarted && (
-        <div className="drawing-controls-top-left">
-          <button
-            onClick={validateAndFinishDrawing}
-            onTouchStart={(e) => {
-              e.preventDefault(); // prevent double trigger
-              validateAndFinishDrawing();
-            }}
-            disabled={!boundary}
-          >
-            Finish Drawing
-          </button>
-          <button className="secondary" onClick={clearBoundary}>Clear Drawing</button>
+      {step === 3 && !drawingStarted && (
+        <div className="overlay overlay-enter">
+          <h2>Where would you mark this neighborhood’s boundaries?</h2>
+          <p>
+            Here's how to draw: <br /><br />
+            1. Tap/click to add a starting point<br />
+            2. Tap/click again to add more points<br />
+            3. Double click/tap to close the shape.<br />
+            4. Click the "Finish Drawing" button when you are done.<br /><br />
+            <strong>Note:</strong> The entirety of a block needs to be within your neighborhood boundary in order to be included. Drawing your boundary along street centerlines will increase accuracy.
+          </p>
+          <div className="overlay-actions">
+            <button onClick={() => {
+              setDrawingStarted(true);
+              drawRef.current.changeMode('draw_polygon');
+            }}>
+              Start Drawing
+            </button>
+          </div>
         </div>
       )}
+
+    {step === 3 && drawingStarted && (
+      <div className="drawing-controls-top-left">
+        <button
+          onClick={validateAndFinishDrawing}
+          onTouchStart={(e) => {
+            e.preventDefault(); // prevent double trigger
+            validateAndFinishDrawing();
+          }}
+          disabled={!boundary}
+        >
+          Finish Drawing
+        </button>
+        <button className="secondary" onClick={clearBoundary}>Clear Drawing</button>
+      </div>
+    )}
 
       {step === 4 && (
         <BoundariesForm
