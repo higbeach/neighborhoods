848af6d71d1bef2802c0a2aa01fe2e09d941051f
Eric Higbee <erichigbee@gmail.com>
2025-10-20 22:20:54 -0700

    h

--- client/src/Map.js
+++ client/src/Map.js
@@ -304,7 +304,7 @@ const startOver = () => {
               e.preventDefault(); // prevent double trigger on iOS
               validateAndFinishDrawing();
             }}
-            disabled={!boundary}
+           // disabled={!boundary}
           >
             Finish Drawing
           </button>
